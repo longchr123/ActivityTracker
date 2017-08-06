41055ebd8fd8ea7293385d49a04b9f37a11f44c5
longchr123 <2805377925@qq.com>
2017-08-06 11:34:20 +0800

    first commit

---
+++ app/src/main/java/com/fashare/activitytracker/MainActivity.java
@@ -0,0 +1,48 @@
+package com.fashare.activitytracker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private static final int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkOverlayPermission();
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(AccessibilityUtil.checkAccessibility(MainActivity.this)) {
+                    startService(
+                            new Intent(MainActivity.this, TrackerService.class)
+                                    .putExtra(TrackerService.COMMAND, TrackerService.COMMAND_OPEN)
+                    );
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                startActivityForResult(
+                        new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()))
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK),
+                        REQUEST_CODE
+                );
+                Toast.makeText(this, "请先授予 \"Activity 栈\" 悬浮窗权限", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
